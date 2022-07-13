b628cd2e248b4e90cdbe138714669408a1f54ab7
eralpkor <eralp.ny@gmail.com>
2022-07-12 20:55:36 -0400

    Add permission based rendering

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -15,76 +15,124 @@ import {
   SimpleShowLayout,
   Show,
   Create,
+  usePermissions,
+  useDelete,
+  FunctionField,
 } from "react-admin";
 import { Divider, Grid } from "@mui/material";
 // custom tooltip to conditianaly render create button
 // usePermission hook every where
-// create beautiful "you are not allowed" component
+// create beautiful "you are not allowed" component +++
 // learn how to use mui grid
-// conditianaly show columns
+// conditianaly show columns +++
 // math the balance and amount paid
-export const JobList = () => (
-  <List>
-    <Datagrid>
-      {/* <TextField source="id" /> */}
-      <TextField source="created_at" />
-      <TextField source="job_title" />
-      <TextField source="job_description" />
-      <TextField source="in_progress" />
-      <DateField source="due_date" />
-      <TextField source="assigned_to" />
-      {false && (
-        <>
-          <TextField source="balance" />
-          <TextField source="amount_paid_1" />
-          <TextField source="amount_paid_2" />
-          <TextField source="amount_paid_2" />
-        </>
-      )}
+const getUserFilters = (permissions) =>
+  [
+    <TextInput label="Job search" source="job_title" alwaysOn />,
+    <TextInput source="username" alwaysOn />,
+    permissions === "admin" ? <TextInput source="assign_to" /> : null,
+  ].filter((filter) => filter !== null);
 
-      <TextField source="customer_id" />
-      <TextField source="user_id" />
-      <TextField source="first_name" />
-      <TextField source="last_name" />
-      <TextField source="username" />
+export const JobList = ({ ...props }) => {
+  const { isLoading, permissions } = usePermissions();
+  console.log("joblist pwrmissions ", permissions);
+  if (isLoading) return null;
 
-      <EditButton />
-    </Datagrid>
-  </List>
-);
+  // DELETE one
+  const DeleteButton = ({ record }) => {
+    const [deleteOne, { isLoading, error }] = useDelete();
+    const handleClick = () => {
+      console.log("record ", record);
+      deleteOne("jobs", { id: record.id, previousData: record });
+    };
+    if (error) {
+      return <p>ERROR</p>;
+    }
+    return (
+      <button disabled={isLoading} onClick={handleClick}>
+        Delete
+      </button>
+    );
+  };
+  return (
+    <List sort={{ field: "job_title", order: "ASC" }}>
+      <Datagrid
+        sx={{
+          "& .column-job_title": { backgroundColor: "#fee" },
+          "& .column-balance": { backgroundColor: "#fee" },
+        }}
+      >
+        {/* <TextField source="id" /> */}
+        {/* <TextField source="created_at" /> */}
+        <TextField source="job_title" />
+        <TextField source="job_description" />
+        <TextField source="in_progress" />
+        <DateField source="due_date" />
+        <TextField source="username" label="Processor" />
 
-export const EditJob = () => (
-  <Edit>
-    <SimpleForm>
-      <Show>
-        <Grid container spacing={2}>
-          <Grid item xs={3}>
-            <SimpleShowLayout spacing={2}>
-              <DateInput source="created_at" label="Created at" />
-              <TextInput source="job_title" label="Job Title" />
-              <TextInput source="in_progress" label="Progress" />
-              <DateInput type="due_date" source="due_date" label="Due Date" />
-            </SimpleShowLayout>
+        {permissions === "admin" && <TextField source="balance" />}
+        {permissions === "admin" && <TextField source="amount_paid_1" />}
+        {permissions === "admin" && <TextField source="amount_paid_2" />}
+        {permissions === "admin" && <TextField source="amount_paid_2" />}
+
+        {/* <TextField source="customer_id" /> */}
+        {/* <TextField source="user_id" /> */}
+        <FunctionField
+          label="Full name"
+          render={(record) => `${record.first_name} ${record.last_name}`}
+        />
+        <TextField source="last_name" />
+        <TextField source="username" />
+        <EditButton />
+      </Datagrid>
+    </List>
+  );
+};
+
+// {...props}
+//       filters={getUserFilters(permissions)}
+//       sort={{ field: "user_id", order: "ASC" }}
+
+export const EditJob = () => {
+  const { isLoading, permissions } = usePermissions();
+  console.log("joblist pwrmissions ", permissions);
+  if (isLoading) return null;
+
+  return (
+    <Edit>
+      <SimpleForm>
+        <Show>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <SimpleShowLayout spacing={2}>
+                {permissions === "admin" && (
+                  <DateInput source="created_at" label="Created at" />
+                )}
+                <TextInput source="job_title" label="Job Title" />
+                <TextInput source="in_progress" label="Progress" />
+                <DateInput source="due_date" label="Due Date" />
+              </SimpleShowLayout>
+            </Grid>
+            <Grid item xs={3}>
+              <SimpleShowLayout spacing={2}>
+                <TextInput source="job_description" label="Description" />
+                <TextInput source="assigned_to" label="Processor: " />
+              </SimpleShowLayout>
+            </Grid>
+            <Grid item xs={3}>
+              <SimpleShowLayout>
+                <TextInput source="balance" label="Balance" />
+                <TextInput source="amount_paid_1" label="Amount paid 1" />
+                <TextInput source="amount_paid_2" label="Amount paid 2" />
+                <TextInput source="amount_paid_3" label="Amount paid 3" />
+              </SimpleShowLayout>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <SimpleShowLayout spacing={2}>
-              <TextInput source="job_description" label="Description" />
-              <TextInput source="assigned_to" label="Assign to: " />
-            </SimpleShowLayout>
-          </Grid>
-          <Grid item xs={3}>
-            <SimpleShowLayout>
-              <TextInput source="balance" label="Balance" />
-              <TextInput source="amount_paid_1" label="Amount paid 1" />
-              <TextInput source="amount_paid_2" label="Amount paid 2" />
-              <TextInput source="amount_paid_3" label="Amount paid 3" />
-            </SimpleShowLayout>
-          </Grid>
-        </Grid>
-      </Show>
-    </SimpleForm>
-  </Edit>
-);
+        </Show>
+      </SimpleForm>
+    </Edit>
+  );
+};
 
 export const CreateJob = () => (
   <Create>
@@ -101,54 +149,7 @@ export const CreateJob = () => (
     </SimpleForm>
   </Create>
 );
-// {
-// 	"id": 3,
-// 	"created_at": "7/1/2022, 5:09:08 PM",
-// 	"job_title": "Movies Maine",
-// 	"job_description": "Analyst Shoes Optional Plastic indexing Plaza Qatari Rial Tennessee vortals Industrial",
-// 	"in_progress": "in-progress",
-// 	"due_date": "7/1/2022, 5:09:08 PM",
-// 	"assigned_to": 3,
-// 	"balance": 250.75,
-// 	"amount_paid_1": 0,
-// 	"amount_paid_2": 0,
-// 	"amount_paid_3": 0,
-// 	"customer_id": 17,
-// 	"user_id": 1
-// }
 
-{
-  /* <Show>
-        <Grid container spacing={2}>
-          <Grid item xs={3}>
-            <TextInput source="job_title" label="Job Title" fullWidth />
-            <TextInput source="in_progress" label="Progress" />
-            <DateInput
-              type="due_date"
-              source="due_date"
-              label="Due Date"
-              defaultValue={new Date()}
-            />
-          </Grid>
-          <Grid item xs={3}></Grid>
-          <TextInput
-            source="first_name"
-            validate={[required()]}
-            label="First Name"
-          />
-          <TextInput source="last_name" label="Last Name" />
-          <TextInput
-            source="username"
-            label="Username"
-            validate={[required()]}
-          />
-          <TextInput
-            type="password"
-            source="password"
-            label="Password"
-            validate={[required()]}
-          />
-          <TextInput source="email" validate={[required()]} label="Email" />
-        </Grid>
-      </Show> */
-}
+export const DeleteJob = () => {
+  return;
+};
