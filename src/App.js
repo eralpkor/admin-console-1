17226d400ce7675971ecdbf1274d2aef9d7106af
eralpkor <eralp.ny@gmail.com>
2022-07-21 19:41:37 -0400

    Add accounts layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import * as React from "react";
-import { Admin, Resource, usePermissions } from "react-admin";
-import { UserList, EditUser, CreateUser } from "./components/Users";
+import { Admin, EditGuesser, Resource, usePermissions } from "react-admin";
+import { UserList } from "./components/Users/UserList";
+import { EditUser } from "./components/Users/EditUser";
+import { CreateUser } from "./components/Users/CreateUser";
 import { JobList } from "./components/jobs/JobList";
-import { EditJob } from "./components/jobs/EditJob";
-import { CreateJob } from "./components/jobs/CreateJob";
+import { JobEdit } from "./components/jobs/JobEdit";
+import { JobCreate } from "./components/jobs/JobCreate";
 import { CustomerList } from "./components/Customers/CustomerList";
 import { EditCustomer } from "./components/Customers/EditCustomer";
 import { CreateCustomer } from "./components/Customers/CreateCustomer";
@@ -13,6 +15,10 @@ import Dashboard from "./components/Dashboard";
 import { authProvider, httpClient } from "./Auth/authProvider";
 import simpleRestProvider from "ra-data-simple-rest";
 import LoginPage from "./components/LoginPage";
+import { MyLayout } from "./MyLayout";
+import { AccountsCreate } from "./components/Accounts/AccountCreate";
+import { AccountsList } from "./components/Accounts/AccountsList";
+import { AccountEdit } from "./components/Accounts/AccountEdit";
 const dataProvider = simpleRestProvider(
   "http://localhost:5000/api",
   httpClient
@@ -21,7 +27,6 @@ const dataProvider = simpleRestProvider(
 const App = () => {
   const { isLoading, permissions } = usePermissions();
 
-  console.log("Permissions in app js ", permissions, isLoading);
   if (isLoading) return null;
 
   return isLoading ? (
@@ -32,6 +37,7 @@ const App = () => {
       dashboard={Dashboard}
       authProvider={authProvider}
       loginPage={LoginPage}
+      layout={MyLayout}
     >
       <Resource
         name="users"
@@ -48,7 +54,14 @@ const App = () => {
         create={CreateCustomer}
         icon={PostIcon}
       />
-      <Resource name="jobs" list={JobList} edit={EditJob} create={CreateJob} />
+      {/* <Resource name="jobs" edit={EditGuesser} /> */}
+      <Resource name="jobs" list={JobList} edit={JobEdit} create={JobCreate} />
+      <Resource
+        name="accounts"
+        list={AccountsList}
+        create={AccountsCreate}
+        edit={AccountEdit}
+      />
     </Admin>
   );
 };
