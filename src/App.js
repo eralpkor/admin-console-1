a79a942d823722523c17b8c23e726c259c51b4a2
eralpkor <eralp.ny@gmail.com>
2022-08-08 12:31:07 -0400

    Add payment and comment components, replaced icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import * as React from "react";
-import { Admin, EditGuesser, Resource, usePermissions } from "react-admin";
+import {
+  Admin,
+  EditGuesser,
+  Resource,
+  usePermissions,
+  useGetIdentity,
+  defaultTheme,
+  ToggleThemeButton,
+} from "react-admin";
 import { UserList } from "./components/Users/UserList";
 import { EditUser } from "./components/Users/EditUser";
 import { CreateUser } from "./components/Users/CreateUser";
@@ -9,8 +17,13 @@ import { JobCreate } from "./components/jobs/JobCreate";
 import { CustomerList } from "./components/Customers/CustomerList";
 import { EditCustomer } from "./components/Customers/EditCustomer";
 import { CreateCustomer } from "./components/Customers/CreateCustomer";
-import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import WorkIcon from "@mui/icons-material/Work";
+import CommentIcon from "@mui/icons-material/Comment";
+
 import Dashboard from "./components/Dashboard";
 import { authProvider, httpClient } from "./Auth/authProvider";
 import simpleRestProvider from "ra-data-simple-rest";
@@ -19,19 +32,38 @@ import { MyLayout } from "./MyLayout";
 import { AccountsCreate } from "./components/Accounts/AccountCreate";
 import { AccountsList } from "./components/Accounts/AccountsList";
 import { AccountEdit } from "./components/Accounts/AccountEdit";
+import { PaymentsList } from "./components/Payments/PaymentsList";
+import { PaymentCreate } from "./components/Payments/PaymentCreate";
+import { PaymentEdit } from "./components/Payments/PaymentEdit";
+import { CommentsList } from "./components/Comments/CommentsList";
+import { JobShow } from "./components/jobs/JobShow";
 const dataProvider = simpleRestProvider(
   "http://localhost:5000/api",
   httpClient
 );
 
+const theme = {
+  ...defaultTheme,
+  palette: {
+    mode: "dark", // Switching the dark mode on is a single property value change.
+  },
+};
+// put in Admin
+// theme={theme}
+
 const App = () => {
-  const { isLoading, permissions } = usePermissions();
+  const { isLoading } = usePermissions();
+  // const { identity, isLoading: identityLoading } = useGetIdentity();
 
-  if (isLoading) return null;
+  // if (identityLoading) return <>Loading...</>;
 
-  return isLoading ? (
-    <div>Waiting for permissions</div>
-  ) : (
+  if (isLoading) return <div>Waiting for permissions</div>;
+  // console.log("App identity ", identity);
+  // return isLoading ? (
+  //   <div>Waiting for permissions</div>
+  // ) : (
+
+  return (
     <Admin
       dataProvider={dataProvider}
       dashboard={Dashboard}
@@ -39,29 +71,53 @@ const App = () => {
       loginPage={LoginPage}
       layout={MyLayout}
     >
-      <Resource
-        name="users"
-        list={UserList}
-        create={CreateUser}
-        edit={EditUser}
-        icon={UserIcon}
-      />
-
-      <Resource
-        name="customers"
-        list={CustomerList}
-        edit={EditCustomer}
-        create={CreateCustomer}
-        icon={PostIcon}
-      />
-      {/* <Resource name="jobs" edit={EditGuesser} /> */}
-      <Resource name="jobs" list={JobList} edit={JobEdit} create={JobCreate} />
-      <Resource
-        name="accounts"
-        list={AccountsList}
-        create={AccountsCreate}
-        edit={AccountEdit}
-      />
+      {/*  Restricting Access */}
+      {(permissions) => [
+        <Resource
+          name="jobs"
+          list={JobList}
+          edit={permissions === "superAdmin" || "admin" ? JobEdit : null}
+          create={permissions === "superAdmin" || "admin" ? JobCreate : null}
+          show={JobShow}
+          icon={WorkIcon}
+        />,
+        <Resource name="comments" list={CommentsList} icon={CommentIcon} />,
+        permissions === "superAdmin" ? (
+          <Resource
+            name="users"
+            list={UserList}
+            create={CreateUser}
+            edit={EditUser}
+            icon={UserIcon}
+          />
+        ) : null,
+        permissions === "superAdmin" || "admin" ? (
+          <Resource
+            name="customers"
+            list={CustomerList}
+            edit={EditCustomer}
+            create={CreateCustomer}
+            icon={PersonOutlineIcon}
+          />
+        ) : null,
+        permissions === "superAdmin" || "admin" ? (
+          <Resource
+            name="accounts"
+            list={AccountsList}
+            create={AccountsCreate}
+            edit={AccountEdit}
+            icon={AccountBalanceIcon}
+          />
+        ) : null,
+        permissions === "superAdmin" || "admin" ? (
+          <Resource
+            name="payments"
+            list={PaymentsList}
+            edit={PaymentEdit}
+            icon={AttachMoneyIcon}
+          />
+        ) : null,
+      ]}
     </Admin>
   );
 };
