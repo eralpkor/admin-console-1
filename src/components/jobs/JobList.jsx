2661c1c0a9c2ea90801ec6b93e30a3daac49afb2
eralpkor <eralp.ny@gmail.com>
2022-08-08 12:37:14 -0400

    Ass job list component

--- src/components/jobs/JobList.jsx
+++ src/components/jobs/JobList.jsx
@@ -9,13 +9,31 @@ import {
   useNotify,
   useRedirect,
   FunctionField,
+  ShowButton,
+  useRecordContext,
+  FilterForm,
+  FilterButton,
+  TextInput,
 } from "react-admin";
+import { Stack } from "@mui/material";
 import { BulkActionButtons } from "../Buttons/BulkActionButtons";
 import { ListActionButtons } from "../Buttons/ListActionButtons";
+
+const filters = [<TextInput label="Title" source="job_title" />];
+
+const ListToolbar = () => (
+  <Stack direction="row" justifyContent="space-between">
+    <FilterForm filters={filters} />
+    <div>
+      <FilterButton filters={filters} />
+    </div>
+  </Stack>
+);
 
 export const JobList = () => {
   const { isLoading, permissions } = usePermissions();
   console.log("joblist pwrmissions ", permissions);
+  const record = useRecordContext();
   const notify = useNotify();
   const redirect = useRedirect();
   if (isLoading) return null;
@@ -23,32 +41,40 @@ export const JobList = () => {
     notify(`Could not load list: ${error.message}`, { type: "warning" });
     redirect("/dashboard");
   };
+  console.log("  rrecord her, ", record);
+  // change the color add to data grid
+  // sx={{
+  //   "& .column-job_title": { backgroundColor: "#fee" },
+  //   "& .column-balance": { backgroundColor: "#fee" },
+  // }}
 
   return (
-    <List actions={<ListActionButtons />} queryOptions={{ onError }}>
+    <List
+      perPage={10}
+      actions={<ListActionButtons />}
+      queryOptions={{ onError }}
+    >
+      {/* <ListToolbar /> */}
       <Datagrid
-        bulkActionButtons={permissions === "admin" && <BulkActionButtons />}
-        sx={{
-          "& .column-job_title": { backgroundColor: "#fee" },
-          "& .column-balance": { backgroundColor: "#fee" },
-        }}
+        bulkActionButtons={
+          permissions === "user" ? null : <BulkActionButtons />
+        }
       >
         <TextField source="id" />
         <TextField source="job_title" />
         <TextField source="job_description" />
         <TextField source="in_progress" />
         <DateField source="due_date" />
-        <TextField source="username" label="Processor" />
-        {permissions === "admin" && <TextField source="balance" />}
-        {permissions === "admin" && <TextField source="amount_paid_1" />}
-        {permissions === "admin" && <TextField source="amount_paid_2" />}
-        {permissions === "admin" && <TextField source="amount_paid_2" />}
+        <TextField source="assigned_to" label="Processor" />
         <FunctionField
           label="Full name"
           render={(record) => `${record.first_name} ${record.last_name}`}
         />
-        <TextField source="username" />
+        {permissions === "user" ? null : <TextField source="total" />}
+        {permissions === "user" ? null : <TextField source="balance" />}
+
         <EditButton />
+        <ShowButton />
       </Datagrid>
     </List>
   );
