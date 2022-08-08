772b931de5cc1d0d54192ce19ca9ddd8d06060eb
eralpkor <eralp.ny@gmail.com>
2022-08-08 12:48:53 -0400

    Add validation

--- src/components/Customers/CreateCustomer.jsx
+++ src/components/Customers/CreateCustomer.jsx
@@ -9,24 +9,6 @@ import {
   maxLength,
 } from "react-admin";
 
-const validateUserCreation = (values) => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = "The firstName is required";
-  }
-  if (!values.age) {
-    // You can return translation keys
-    errors.age = "ra.validation.required";
-  } else if (values.age < 18) {
-    // Or an object if the translation messages need parameters
-    errors.age = {
-      message: "ra.validation.minValue",
-      args: { min: 18 },
-    };
-  }
-  return errors;
-};
-
 export const CreateCustomer = () => {
   const validateEmail = email();
   const validateFirstName = [required(), minLength(2), maxLength(15)];
