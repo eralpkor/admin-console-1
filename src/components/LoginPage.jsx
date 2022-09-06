3b0b3f5de96cf9f1bde6dc5b348b137fd0886849
eralpkor <eralp.ny@gmail.com>
2022-09-06 14:51:03 -0400

    add copyright component

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -14,24 +14,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://aexperts.com/">
-        Accounting Experts
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import { Copyright } from "./Helpers/Copyright";
 
 const theme = createTheme();
 
