bd127b663aa9b6fb3e412e56824f53cd6507916d
eralpkor <eralp.ny@gmail.com>
2022-10-04 23:03:03 -0400

    Add filter/search and permissions

--- src/components/jobs/JobList.jsx
+++ src/components/jobs/JobList.jsx
@@ -8,31 +8,29 @@ import {
   usePermissions,
   useNotify,
   useRedirect,
-  FunctionField,
-  ShowButton,
   useRecordContext,
   FilterForm,
   FilterButton,
   TextInput,
+  NumberField,
+  ReferenceField,
+  CloneButton,
 } from "react-admin";
 import { Stack } from "@mui/material";
 import { BulkActionButtons } from "../Buttons/BulkActionButtons";
 import { ListActionButtons } from "../Buttons/ListActionButtons";
+import { FullNameField } from "../Helpers/FullName";
 
-const filters = [<TextInput label="Title" source="job_title" />];
+const filters = [
+  <TextInput autoFocus label="Processor" source="username" />,
+  <TextInput autoFocus label="Title" source="title" />,
+  <TextInput autoFocus label="Last name" source="lastName" />,
+  <TextInput autoFocus label="In-Progress" source="inProgress" />,
+];
 
-const ListToolbar = () => (
-  <Stack direction="row" justifyContent="space-between">
-    <FilterForm filters={filters} />
-    <div>
-      <FilterButton filters={filters} />
-    </div>
-  </Stack>
-);
-
-export const JobList = () => {
+export const JobList = (props) => {
   const { isLoading, permissions } = usePermissions();
-  console.log("joblist pwrmissions ", permissions);
+  console.log("joblist permissions ", permissions);
   const record = useRecordContext();
   const notify = useNotify();
   const redirect = useRedirect();
@@ -41,40 +39,73 @@ export const JobList = () => {
     notify(`Could not load list: ${error.message}`, { type: "warning" });
     redirect("/dashboard");
   };
-  console.log("  rrecord her, ", record);
+  console.log("record, ", record);
   // change the color add to data grid
   // sx={{
-  //   "& .column-job_title": { backgroundColor: "#fee" },
+  //   "& .column-title": { backgroundColor: "#fee" },
   //   "& .column-balance": { backgroundColor: "#fee" },
   // }}
+  const FilterToolbar = () => (
+    <Stack direction="row" justifyContent="space-between">
+      <FilterForm filters={filters} />
+      <div>
+        <FilterButton filters={filters} />
+      </div>
+    </Stack>
+  );
 
   return (
     <List
+      {...props}
       perPage={10}
       actions={<ListActionButtons />}
       queryOptions={{ onError }}
     >
-      {/* <ListToolbar /> */}
+      <FilterToolbar />
       <Datagrid
+        rowClick="edit"
         bulkActionButtons={
-          permissions === "user" ? null : <BulkActionButtons />
+          permissions === "USER" ? null : <BulkActionButtons />
         }
+        sx={{
+          "& .column-title": { backgroundColor: "#fee" },
+          "& .column-balance": { backgroundColor: "#fee" },
+        }}
       >
         <TextField source="id" />
-        <TextField source="job_title" />
-        <TextField source="job_description" />
-        <TextField source="in_progress" />
-        <DateField source="due_date" />
-        <TextField source="assigned_to" label="Processor" />
-        <FunctionField
-          label="Full name"
-          render={(record) => `${record.first_name} ${record.last_name}`}
-        />
-        {permissions === "user" ? null : <TextField source="total" />}
-        {permissions === "user" ? null : <TextField source="balance" />}
+        <TextField source="title" />
+        <TextField maxWidth={250} source="description" />
+        {/* Customer name */}
+        <FullNameField />
+        <TextField source="inProgress" />
+        <DateField source="dueDate" />
+        <TextField source="username" label="Processor" />
+        <ReferenceField label="Admin" source="adminId" reference="user">
+          <TextField source="username" />
+        </ReferenceField>
 
-        <EditButton />
-        <ShowButton />
+        {permissions === "USER" ? null : (
+          <NumberField
+            source="total"
+            options={{
+              style: "currency",
+              currency: "USD",
+              maximumFractionDigits: 2,
+            }}
+          />
+        )}
+        {permissions === "USER" ? null : (
+          <NumberField
+            source="balance"
+            options={{
+              style: "currency",
+              currency: "USD",
+              maximumFractionDigits: 2,
+            }}
+          />
+        )}
+
+        {/* <EditButton /> */}
       </Datagrid>
     </List>
   );
