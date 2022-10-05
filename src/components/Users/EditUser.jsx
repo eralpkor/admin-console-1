2b9e92581cbe3e0bd1b851c9fd378d6cb9970bc5
eralpkor <eralp.ny@gmail.com>
2022-10-04 23:04:51 -0400

    Add edit user component

---
+++ src/components/Users/EditUser.jsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import {
+  useRecordContext,
+  Edit,
+  SimpleForm,
+  TextInput,
+  required,
+  useAuthenticated,
+  usePermissions,
+  ReferenceInput,
+  SelectInput,
+  minLength,
+  maxLength,
+  email,
+  useGetIdentity,
+} from "react-admin";
+import MyUrlField from "../Helpers/MyUrlField";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import StickyFooter from "../Helpers/NoAcess";
+
+const GetTitle = () => {
+  const record = useRecordContext();
+  const { identity, isLoading: identityLoading } = useGetIdentity();
+  if (identityLoading) return <>Loading...</>;
+  // console.log("Identity ", identity);
+  console.log("Edit title ", record); // record = job
+
+  return (
+    <span>User {record ? `"${record.firstName} ${record.lastName}"` : ""}</span>
+  );
+};
+
+export const EditUser = () => {
+  const validateEmail = email();
+  const validateFirstName = [required(), minLength(2), maxLength(15)];
+  return (
+    <Edit title={<GetTitle />}>
+      <SimpleForm>
+        <TextInput
+          source="firstName"
+          label="First Name"
+          validate={validateFirstName}
+        />
+        <TextInput
+          source="lastName"
+          label="Last Name"
+          validate={validateFirstName}
+        />
+        <TextInput source="username" label="Username" validate={required()} />
+        <TextInput
+          type="password"
+          source="password"
+          label="Password"
+          validate={required()}
+        />
+        <TextInput
+          size="large"
+          source="email"
+          label="Email"
+          type="email"
+          validate={validateEmail}
+        />
+        <SelectInput
+          validate={required()}
+          //optionText={(record) => record.role_id}
+          source="role"
+          label="User role"
+          choices={[
+            { id: "SUPERADMIN", name: "SUPERADMIN" },
+            { id: "ADMIN", name: "ADMIN" },
+            { id: "USER", name: "USER" },
+          ]}
+        />
+      </SimpleForm>
+    </Edit>
+  );
+};
