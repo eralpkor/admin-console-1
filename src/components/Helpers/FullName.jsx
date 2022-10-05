4a01ea7dca7a54880f2ec979061467515fef953a
eralpkor <eralp.ny@gmail.com>
2022-10-04 23:08:43 -0400

    Add Full Name helper component

--- src/components/Helpers/FullName.jsx
+++ src/components/Helpers/FullName.jsx
@@ -2,7 +2,7 @@ import { useRecordContext } from "react-admin";
 
 export const FullNameField = (props) => {
   const record = useRecordContext(props);
-  console.log("whats is record in function ", record);
+
   return record ? (
     <span>
       {record.firstName} {record.lastName}
