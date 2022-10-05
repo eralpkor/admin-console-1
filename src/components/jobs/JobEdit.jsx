2497742b07106fbdf883a4b183702c6089131d63
eralpkor <eralp.ny@gmail.com>
2022-10-04 23:01:10 -0400

    Add clone button and referencemanyfield components

--- src/components/jobs/JobEdit.jsx
+++ src/components/jobs/JobEdit.jsx
@@ -6,313 +6,305 @@ import {
   useRedirect,
   Edit,
   SimpleForm,
-  // TextField,
   TextInput,
   DateInput,
   SelectInput,
   Labeled,
   useGetIdentity,
-  useCreate,
-  AutocompleteInput,
+  DateField,
   ReferenceInput,
-  EditButton,
   Datagrid,
-  ListBase,
-  useCreateTitle,
-  Pagination,
-  FilterForm,
-  Title,
   required,
-  useGetList,
-  ListContextProvider,
-  useList,
+  NumberField,
+  ReferenceManyField,
+  TextField,
+  ReferenceField,
 } from "react-admin";
-import { Button, Card, TextField, TextareaAutosize } from "@mui/material";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  TextareaAutosize,
+  Box,
+  Divider,
+} from "@mui/material";
 import { EditRecordButtons } from "../Buttons/EditRecordButtons";
-
-// Add comment area and button
-const AddComment = () => {
-  const notify = useNotify();
-  const [create, { isLoading, error }] = useCreate();
-  const record = useRecordContext();
-  const [comment, setComment] = React.useState("");
-  const redirect = useRedirect();
-
-  const handleChange = (e) => {
-    setComment(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("comment", comment);
-    console.log("record ", record);
-    const data = { job_id: record.id, comment: comment };
-    create("comments", { data: data });
-    setComment("");
-    notify("Comment added...");
-    redirect(`/jobs/${record.id}`);
-    // window.location.reload();
-  };
-
-  if (error) return <p>ERROR</p>;
-
-  return (
-    <>
-      <TextareaAutosize
-        onChange={handleChange}
-        value={comment}
-        id="comment"
-        name="comment"
-        aria-label="minimum height"
-        minRows={3}
-        placeholder="Please add a comment"
-        style={{ width: 400 }}
-      />
-      <Button disabled={isLoading} onClick={handleSubmit} variant="contained">
-        Add Comment
-      </Button>
-    </>
-  );
-};
-
-const CommentList = () => {
-  const { data, isLoading } = useGetList("comments", { page: 1, perPage: 50 });
-  const record = useRecordContext();
-  // console.log("whats comments ", record);
-  const listContext = useList({ data, isLoading });
-  // console.log("whats data ", data, isLoading);
-  if (isLoading) return null;
-  const filteredComments = data.filter((c, i) => {
-    return c.job_id === record.id;
-  });
-  // console.log("whats length ", filteredComments.length);
-  return (
-    <div>
-      <ListContextProvider value={listContext}>
-        <ul style={{ listStyleType: "none", marginLeft: -10 }}>
-          {!record.comments ? (
-            <span style={{ color: "red" }}>
-              No comments, would you like to add one?
-            </span>
-          ) : (
-            record.comments.map((c, i) => {
-              return <li key={i}>{c.comment}</li>;
-            })
-          )}
-        </ul>
-      </ListContextProvider>
-    </div>
-  );
-};
-
-const PaymentList = () => {
-  const { data, isLoading } = useGetList("payments", { page: 1, perPage: 50 });
-
-  const record = useRecordContext();
-  if (isLoading) return null;
-
-  return (
-    <>
-      <ul style={{ listStyleType: "none", marginLeft: -10 }}>
-        {!record.payments ? (
-          <span style={{ color: "red" }}>
-            No comments, would you like to add one?
-          </span>
-        ) : (
-          record.payments.map((c, i) => {
-            return <li key={i}>$ {c.amount_paid}</li>;
-          })
-        )}
-      </ul>
-    </>
-  );
-};
-
+import { FullNameField } from "../Helpers/FullName";
+import "./jobEdit.css";
+import { ListActionButtons } from "../Buttons/ListActionButtons";
 // Show JOB title
 const EditTitle = () => {
   const record = useRecordContext();
   const { identity, isLoading: identityLoading } = useGetIdentity();
   if (identityLoading) return <>Loading...</>;
-  // console.log("Identity ", identity);
-  // console.log("Edit title ", record); // record = job
 
-  return <span>Job {record ? `"${record.job_title}"` : ""}</span>;
+  return <span>Job {record ? `"${record.title}"` : ""}</span>;
 };
 
-// Add Payment input & button
-const AddPayment = (e) => {
-  const notify = useNotify();
-  const [create, { isLoading, error }] = useCreate();
+// Add new Comment button
+const CreateCommentButton = () => {
   const record = useRecordContext();
-  const redirect = useRedirect();
-  const [payment, setPayment] = React.useState({
-    payment_type: "",
-    check_number: "",
-    amount_paid: "",
-    account_id: record.id,
-  });
+  const { identity, isLoading: identityLoading } = useGetIdentity();
+  if (identityLoading) return <>Loading...</>;
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    const { name, value } = e.target;
-    setPayment((payment) => ({
-      ...payment,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = () => {
-    console.log("comment", payment);
-    console.log("record ", record);
-    const data = {
-      payment_type: payment.payment_type,
-      check_number: payment.check_number,
-      amount_paid: +payment.amount_paid,
-      account_id: record.id,
-    };
-    create("payments", { data: data });
-    console.log("What is payment data ", data);
-    setPayment({
-      payment_type: "",
-      check_number: "",
-      amount_paid: "",
-      account_id: record.id,
-    });
-    if (!error) {
-      notify("Payment added...");
-      // redirect(`/accounts/${record.id}`);
-      window.location.reload();
-    }
-  };
-
-  if (error) return <p>ERROR</p>;
   return (
-    <>
-      <TextField
-        onChange={handleChange}
-        margin="normal"
-        validate={[required()]}
-        required
-        id="payment_type"
-        label="Payment type"
-        name="payment_type"
-        autoFocus
-      />
-      <TextField
-        onChange={handleChange}
-        margin="normal"
-        validate={[required()]}
-        required
-        id="check_number"
-        label="Check number"
-        name="check_number"
-      />
-      <TextField
-        onChange={handleChange}
-        margin="normal"
-        validate={[required()]}
-        required
-        id="amount_paid"
-        label="Payment"
-        name="amount_paid"
-      />
-      <Button disabled={isLoading} onClick={handleSubmit} variant="contained">
-        Add Payment
-      </Button>
-    </>
+    <Button
+      sx={{ marginTop: "1rem" }}
+      variant="contained"
+      component={Link}
+      to={{
+        pathname: "/comment/create",
+      }}
+      state={{
+        record: {
+          jobId: record.id,
+          editedBy: identity.id,
+          userId: identity.id,
+        },
+      }}
+    >
+      Create a new comment for this job
+    </Button>
   );
 };
 
-export const JobEdit = () => {
+// Add new payment button
+const CreatePaymentButton = () => {
+  const record = useRecordContext();
+  const { identity, isLoading: identityLoading } = useGetIdentity();
+  if (identityLoading) return <>Loading...</>;
+
+  return (
+    <Button
+      sx={{ marginTop: "1rem" }}
+      variant="contained"
+      component={Link}
+      to={{
+        pathname: "/payment/create",
+      }}
+      state={{
+        record: {
+          jobId: record.id,
+          editedBy: identity.id,
+          userId: identity.id,
+        },
+      }}
+    >
+      Create a new payment for this job
+    </Button>
+  );
+};
+
+export const JobEdit = (props) => {
+  const record = useRecordContext();
   const { isLoading, permissions } = usePermissions();
   const notify = useNotify();
   const redirect = useRedirect();
   const { identity, isLoading: identityLoading } = useGetIdentity();
   if (identityLoading) return <>Loading...</>;
-
-  // console.log("What is user permission ", permissions);
+  console.log("identity", identity);
   if (isLoading) return null;
   const onError = (error) => {
     notify(`Could not load list: ${error.message}`, { type: "warning" });
     redirect("/dashboard");
   };
-
-  // mutationOptions={{ onSuccess }}
-  return permissions === "user" ? (
+  console.log("record edit ", record);
+  return permissions === "USER" ? (
     // user inputs
-    <Edit title={<EditTitle />} queryOptions={{ onError }}>
+    <Edit {...props} title={<EditTitle />} queryOptions={{ onError }}>
       <SimpleForm warnWhenUnsavedChanges toolbar={<EditRecordButtons />}>
         <TextInput disabled source="id" label="Job ID" />
-        <TextInput disabled source="job_title" label="Job Title" />
+        <TextInput disabled source="title" label="Job Title" />
         <TextInput
           disabled
-          source="job_description"
+          source="description"
           label="Description"
           multiline={true}
           fullWidth
         />
-        <TextInput disabled source="due_date" label="Due Date" />
-        <ReferenceInput source="assigned_to" reference="users">
-          <SelectInput disabled label="Processor" optionText="username" />
-        </ReferenceInput>
+        <Labeled label="Due date">
+          <DateField disabled source="dueDate" label="Due Date" />
+        </Labeled>
+
+        <TextInput disabled source="processor" />
         <SelectInput
-          source="in_progress"
+          source="inProgress"
           label="Progress"
           choices={[
-            { id: "open", name: "Open" },
-            { id: "in-progress", name: "In-Progress" },
-            { id: "closed", name: "Closed" },
+            { id: "OPEN", name: "OPEN" },
+            { id: "INPROGRESS", name: "IN-PROGRESS" },
+            { id: "CLOSED", name: "CLOSED" },
           ]}
         />
-        <TextInput disabled source="first_name" />
-        <TextInput disabled source="last_name" />
-        <Labeled label="Username">
+
+        <ReferenceInput source="customerId" reference="customer">
+          <SelectInput disabled optionText={<FullNameField />} />
+        </ReferenceInput>
+
+        <Labeled label="Admin">
           <TextInput disabled source={identity.username} />
         </Labeled>
-        <CommentList />
-        <AddComment />
+        {/* Comments  */}
+        <Divider />
+        <Card>
+          <h3 style={{ color: "#DC143C" }}>Comment/s</h3>
+
+          <ReferenceManyField
+            reference="comment"
+            label="Comment"
+            target="jobId"
+            perPage={100}
+          >
+            <Datagrid rowClick="edit">
+              <DateField source="createdAt" sortByOrder="DESC" />
+              <DateField source="updatedAt" />
+              <TextField source="comment" />
+
+              <TextField label="Processor" source="userId" />
+              <TextField source="editedBy" />
+              <NumberField source="jobId" />
+            </Datagrid>
+          </ReferenceManyField>
+          <CreateCommentButton />
+        </Card>
       </SimpleForm>
     </Edit>
   ) : (
     // Admin inputs
     <Edit title={<EditTitle />} queryOptions={{ onError }}>
       <SimpleForm toolbar={<EditRecordButtons />}>
-        <TextInput disabled source="id" label="Job ID" />
-        <TextInput source="job_title" label="Job Title" />
+        <Card variant="outlined" sx={{ maxWidth: 345 }}>
+          {/* <CardActionArea> */}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              <TextInput disabled source="id" label="Job ID" />
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              <Labeled label="Created at">
+                <DateField source="createdAt" label="Created at" />
+              </Labeled>
+              <span> / </span>
+              <Labeled label="Edited at">
+                <DateField source="updatedAt" label="Updated at" />
+              </Labeled>
+            </Typography>
+          </CardContent>
+          {/* </CardActionArea> */}
+        </Card>
+
+        <TextInput source="title" label="Job Title" />
         <TextInput
-          source="job_description"
+          source="description"
           label="Description"
-          multiline={true}
+          multiline
+          resettable
           fullWidth
         />
-        <DateInput source="due_date" label="Due Date" />
-        <ReferenceInput source="assigned_to" reference="users">
-          <SelectInput label="Processor" optionText="username" />
+        <DateInput source="dueDate" label="Due Date" />
+
+        <ReferenceInput
+          label="User"
+          source="userId"
+          reference="user"
+          validate={[required()]}
+        >
+          <SelectInput optionText="username" />
+        </ReferenceInput>
+        <ReferenceInput source="customerId" reference="customer">
+          <SelectInput optionText={<FullNameField />} />
         </ReferenceInput>
         <SelectInput
-          source="in_progress"
+          source="inProgress"
           label="Progress"
           choices={[
-            { id: "open", name: "Open" },
-            { id: "in-progress", name: "In-Progress" },
-            { id: "closed", name: "Closed" },
+            { id: "OPEN", name: "OPEN" },
+            { id: "INPROGRESS", name: "INPROGRESS" },
+            { id: "CLOSED", name: "CLOSED" },
           ]}
         />
-        <TextInput source="total" />
-        <TextInput disabled source="balance" label="Balance $" />
-        <p>All payments for this job</p>
-        <PaymentList />
-        <AddPayment />
-        <TextInput disabled source="first_name" />
-        <TextInput disabled source="last_name" />
-        <TextInput disabled source="created_at" label="Created at" />
-        <TextInput disabled source="updated_at" label="Updated at" />
-        <Labeled label="Username">
-          <TextInput disabled source={identity.username} />
+        <TextInput label="Total $" source="total" />
+        <Labeled label="Balance">
+          <NumberField
+            sx={{ fontSize: "1.5rem" }}
+            source="balance"
+            options={{
+              style: "currency",
+              currency: "USD",
+              maximumFractionDigits: 2,
+            }}
+          />
         </Labeled>
-        <CommentList />
-        <AddComment />
+        {/* Payment list */}
+
+        <Card>
+          <h3 style={{ color: "#DC143C" }}>Payment/s</h3>
+          <ReferenceManyField
+            reference="payment"
+            label="Payment"
+            target="jobId"
+            perPage={100}
+          >
+            <Datagrid rowClick="edit">
+              <NumberField source="id" />
+              <DateField source="createdAt" sortByOrder="DESC" />
+              <DateField source="updatedAt" />
+              {/*         */}
+              <TextField source="editedBy" label="Admin" />
+              {/* <NumberField source="jobId" /> */}
+              <TextField source="paymentType" />
+              <NumberField
+                sx={{ fontSize: "0.8rem" }}
+                source="amountPaid"
+                options={{
+                  style: "currency",
+                  currency: "USD",
+                  maximumFractionDigits: 2,
+                }}
+              />
+            </Datagrid>
+          </ReferenceManyField>
+          {/* Payment add  */}
+          <CreatePaymentButton />
+        </Card>
+        {/* Comment list  */}
+        <Divider />
+        <Card>
+          <h3 style={{ color: "#DC143C" }}>Comment/s</h3>
+          <ReferenceManyField
+            reference="comment"
+            label="Comment"
+            target="jobId"
+            perPage={100}
+          >
+            <Datagrid rowClick="edit">
+              <DateField source="createdAt" sortByOrder="DESC" />
+              <DateField source="updatedAt" />
+              <TextField source="comment" />
+
+              <ReferenceField
+                label="Processor"
+                source="userId"
+                reference="user"
+              >
+                <TextField source="username" />
+              </ReferenceField>
+              <ReferenceField label="Admin" source="editedBy" reference="user">
+                <TextField source="username" />
+              </ReferenceField>
+            </Datagrid>
+          </ReferenceManyField>
+          <CreateCommentButton />
+        </Card>
+        <Divider sx={{ marginTop: "1rem" }} />
+
+        <Labeled label="Admin">
+          <TextField disabled source="adminId" defaultValue={identity.id} />
+        </Labeled>
       </SimpleForm>
     </Edit>
   );
 };
+
+// EOF
