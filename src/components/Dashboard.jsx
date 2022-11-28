79d92df97fb7f41343408db920d5185e0bcabc2a
eralpkor <eralp.ny@gmail.com>
2022-11-28 15:22:33 -0500

    Add total user jobs and new greeting

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,22 +1,61 @@
 import * as React from "react";
+import {
+  useGetIdentity,
+  useGetMany,
+  useGetList,
+  List,
+  Datagrid,
+  TextField,
+  useNotify,
+  useAuthenticated,
+  usePermissions,
+  EmailField,
+  useRedirect,
+} from "react-admin";
+import { Link } from "react-router-dom";
 import { Card, CardContent, CardHeader } from "@mui/material";
 import { UserName } from "../Auth/userAuth";
 
 const date = new Date();
 let hours = date.getHours();
 let status =
-  hours < 12 ? "Morning" : hours <= 18 && hours >= 12 ? "Afternoon" : "Night";
+  hours < 12 ? "Morning" : hours <= 18 && hours >= 12 ? "Afternoon" : "Evening";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => (
-  <Card>
-    <CardHeader title="Welcome to the administration" />
-    <CardContent>
-      Welcome{" "}
-      <strong>
-        <UserName />
-      </strong>
-      <span id="day-message">, Good {status}.</span>
-    </CardContent>
-  </Card>
-);
+export default () => {
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  // const { identity, isLoading, error } = useGetIdentity();
+  // const { identity } = useGetIdentity();
+  // const { data, refetch } = useGetMany("user-job", { userId: [1] });
+  const { data, total, isLoading, error } = useGetList("user-job", {
+    filter: { userId: auth.id },
+  });
+  if (isLoading) return <div>Loading</div>;
+  if (error) return <div>error;</div>;
+
+  return (
+    <Card>
+      <CardHeader title="Welcome to the administration" />
+      <CardContent>
+        Welcome{" "}
+        <strong>
+          <UserName />
+        </strong>
+        <span id="day-message">, Good {status}.</span>
+        <Link
+          to="/user-job"
+          sx={{
+            textDecoration: "none",
+          }}
+        >
+          <span>
+            {" "}
+            <strong>
+              {data.length ? ` You have ${data.length} jobs` : " "}
+            </strong>
+          </span>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
